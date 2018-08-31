b12e277ef928426ecd567d74a81334bda1b534bc
MikeG212 <michaelcgee93@gmail.com>
2018-08-31 08:28:46 -0700

    Remove Mega Problem Set file

--- Mega Problem Set Assessment 1/rec_sum.rb
+++
@@ -1,8 +0,0 @@
-# Write a recursive method that returns the sum of all elements in an array
-def rec_sum(nums)
-  return nums[0] if nums.length == 1 #1
-  nums.last + rec_sum(nums[0...-1]) #2
-end
-
-#1 base case: return the first element array if it is a 1 element array
-#2 add last number of array with calling rec_sum(on the array, all but last num)
--- Mega Problem Set Assessment 1/subsets_trial.rb
+++
@@ -1,5 +0,0 @@
-def subsets(arr)
-  return [[]] if arr.length < 1
-  subs = arr[0..-2]
-  subs.concat(subs.map { |el| el += arr.last } )
-end
