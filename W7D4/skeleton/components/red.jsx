84cba0670328102db625261eadbbe6528e9bfb8b
MikeG212 <michaelcgee93@gmail.com>
2018-10-04 08:56:10 -0700

    Build Rainbow Router

---
+++ W7D4/skeleton/components/red.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        {/* Links here */}
+
+        {/* Routes here */}
+        <Route path="/red/orange" component={Orange} />
+      </div>
+    );
+  }
+};
+
+export default Red;
